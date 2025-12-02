9fb7e7088dc7a0b1200460a1e89f37fc1512f23f
Николай <usenkko.nikolay@gmail.com>
2025-12-02 12:43:22 +0300

    Add and update for task 2

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -9,46 +9,21 @@ namespace software_lab9
             InitializeComponent();
         }
 
-        private void Calc_CLick(object sender, RoutedEventArgs e)
+        private void Button_Click(object sender, RoutedEventArgs e)
         {
-            if (Radio1.IsChecked.GetValueOrDefault())
+            double x = Convert.ToDouble(TextX.Text);
+            double y = Convert.ToDouble(TextY.Text);
+            int n = Convert.ToInt32(ComboN.Text);
+            int k = Convert.ToInt32(ComboK.Text);
+            double res = 0;
+            for (int i = 1; i < n; i++)
             {
-                double a = Convert.ToDouble(R1TextA.Text);
-                double f = Convert.ToDouble(R1ComboF.Text);
-                this.Title = "Ответ: " + Math.Sin(f * a).ToString("F");
-            }
-
-            if (Radio2.IsChecked.GetValueOrDefault())
-            {
-                double a = Convert.ToDouble(R2TextA.Text);
-                double b = Convert.ToDouble(R2TextB.Text);
-                double f = Convert.ToDouble(R2ComboF.Text);
-                this.Title = "Ответ: "
-                    + (Math.Cos(f * a) + Math.Sin(f * b)).ToString("F");
-            }
-
-            if (Radio3.IsChecked.GetValueOrDefault())
-            {
-                double a = Convert.ToDouble(R3TextA.Text);
-                double b = Convert.ToDouble(R3TextB.Text);
-                double c = Convert.ToDouble(R3ComboC.Text);
-                double d = Convert.ToDouble(R3ComboD.Text);
-                this.Title = "Ответ: "
-                    + (c * Math.Pow(a, 2) + d * Math.Pow(b, 2)).ToString("F");
-            }
-
-            if (Radio4.IsChecked.GetValueOrDefault())
-            {
-                double a = Convert.ToDouble(R4TextA.Text);
-                double d = Convert.ToDouble(R4TextD.Text);
-                double c = Convert.ToDouble(R4ComboC.Text);
-                double res = 1;
-                for (int i = 0; i < d; i++)
+                for (int j = 1; j < k; j++)
                 {
-                    res = res * (c + a) + 1;
+                    res += (i * Math.Pow(x, i) + j * Math.Pow(y, j)) / (i * j);
                 }
-                this.Title = "Ответ: " + res.ToString("F");
             }
+            this.Title = "Ответ: " + res.ToString("F");
         }
     }
 }
